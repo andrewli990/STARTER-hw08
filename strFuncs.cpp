67921b343cf5907039071ba093461efaaaffa791
andrewli990 <andrewli990@ucsb.edu>
2025-12-05 21:33:08 -0800

    Update strFuncs.cpp

--- strFuncs.cpp
+++ strFuncs.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 /* Precondition: s1 is a valid string that may contain upper or lower case alphabets, no spaces or special characters
  * Postcondition: Returns true if s1 is a palindrome, false otherwise
  *
@@ -11,6 +10,11 @@ using namespace std;
  *
  *You should provide a recursive solution*/
 bool isPalindrome(const string s1){
-    //STUB: Replace the following with the correct code.
-    return true;
+    if(s1.length() <= 1){
+        return true;
+    }
+    if(tolower(s1[0]) != tolower(s1[s1.length()-1])) {
+        return false;
+    }
+    return isPalindrome(s1.substr(1,s1.length()-2));
 }
