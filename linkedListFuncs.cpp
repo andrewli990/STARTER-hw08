dbefb0d8e39b10f0487e0257568ce8db6956c1b8
andrewli990 <andrewli990@ucsb.edu>
2025-12-05 21:58:14 -0800

    Update linkedListFuncs.cpp

--- linkedListFuncs.cpp
+++ linkedListFuncs.cpp
@@ -20,7 +20,13 @@ int recursiveSum(Node* head) {
 //return the largest value in the linked list using a recursive approach
 //you may assume the list has at least one element
 int recursiveLargestValue(Node* head) {
-
+  if(!head){
+    return 0;
+  }
+  else
+  {
+    return head->data + recursiveSum(head->next);
+  }
   return -42;
 }
 
